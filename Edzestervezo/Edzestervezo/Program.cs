f9e7e43c476e68db87c626a10330d666482edc34
Nkrs3 <b3nagkri@vasvari.org>
2025-10-20 19:07:55 +0200

    e

--- Edzestervezo/Edzestervezo/Program.cs
+++ Edzestervezo/Edzestervezo/Program.cs
@@ -25,6 +25,25 @@ namespace FitProgram
                 Console.WriteLine("Hiba! A névnek nagybetűvel kell kezdődnie.");
             }
 
+            while (true)
+            {
+                Console.Write("Add meg a keresztneved (Nagy kezdőbetű): ");
+                kernev = Console.ReadLine();
+                if (!string.IsNullOrEmpty(kernev) && char.IsUpper(kernev[0]))
+                    break;
+
+                Console.WriteLine("Hiba! A névnek nagybetűvel kell kezdődnie.");
+            }
+
+            
+            while (true)
+            {
+                Console.Write("Add meg a testsúlyod (50-120 kg): ");
+                if (int.TryParse(Console.ReadLine(), out testsuly) && testsuly >= 50 && testsuly <= 120)
+                    break;
+
+                Console.WriteLine("Hibás érték! Csak 50 és 120 kg közötti szám lehet.");
+            }
 
 
         }
