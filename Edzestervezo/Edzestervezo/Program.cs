a0ca76dfef2ffd2ace1f1d1c205bfcf75e7ac2d9
Nkrs3 <b3nagkri@vasvari.org>
2025-10-20 19:02:23 +0200

    e

---
+++ Edzestervezo/Edzestervezo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FitProgram
+{
+    internal class WorkoutPlanner
+    {
+        static void Main(string[] args)
+        {
+            string veznev;
+            string kernev;
+            int testsuly = 0;
+            int fitcel = 0;
+            double alapido = 0;
+            double kalszorzo = 0;
+            string[] cellista = { "Állóképesség javítása", "Izomtömeg növelése", "Testsúlycsökkentés" };
+
+
+            while (true)
+            {
+                Console.Write("Add meg a vezetékneved (Nagy kezdőbetű): ");
+                veznev = Console.ReadLine();
+                if (!string.IsNullOrEmpty(veznev) && char.IsUpper(veznev[0]))
+                    break;
+
+                Console.WriteLine("Hiba! A névnek nagybetűvel kell kezdődnie.");
+            }
+
+
+
+        }
+    }
+}
